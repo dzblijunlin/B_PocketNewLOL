f1cec895a8cb7025f230764029a7c2aa816c4291
史强 <3282035164@qq.com>
2016-12-20 20:43:17 +0800

    一节界面

---
+++ B_PocketNewLOL/app/src/main/java/com/naruto/b_pocketnewlol/news/GlideImageLoader.java
@@ -0,0 +1,20 @@
+package com.naruto.b_pocketnewlol.news;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by dllo on 16/11/24.
+ */
+
+public class GlideImageLoader extends ImageLoader {
+
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        Glide.with(context).load(path).into(imageView);
+    }
+}
