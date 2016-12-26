1d0af306e11d7a983294f42743beadaffe96fff9
大嘴宝 <friendforever14@vip.qq.com>
2016-12-26 17:17:12 +0800

    123

--- B_PocketNewLOL/app/src/main/java/com/naruto/b_pocketnewlol/discovery/activity/TeamSecondActivity.java
+++ B_PocketNewLOL/app/src/main/java/com/naruto/b_pocketnewlol/discovery/activity/TeamSecondActivity.java
@@ -2,13 +2,19 @@ package com.naruto.b_pocketnewlol.discovery.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
 import com.github.jdsjlzx.recyclerview.LRecyclerView;
 import com.github.jdsjlzx.recyclerview.LRecyclerViewAdapter;
+import com.github.jdsjlzx.recyclerview.ProgressStyle;
 import com.naruto.b_pocketnewlol.R;
 import com.naruto.b_pocketnewlol.base.BaseActivity;
 import com.naruto.b_pocketnewlol.discovery.adapter.TeamPhotoAdapter;
 import com.naruto.b_pocketnewlol.discovery.bean.TeamNumPhotoBean;
+import com.naruto.b_pocketnewlol.entity.NetTool;
+import com.naruto.b_pocketnewlol.entity.onHttpCallBack;
+import com.naruto.b_pocketnewlol.tools.UrlTools;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +22,11 @@ import java.util.List;
 public class TeamSecondActivity extends BaseActivity {
 
     private List<TeamNumPhotoBean.MembersBean> data;
-    private LRecyclerView lRecyclerView;
+    private LRecyclerView rv;
     private LRecyclerViewAdapter lRecyclerViewAdapter;
-    private TeamPhotoAdapter photoAdapter;
+
+    private TeamPhotoAdapter adapter;
+
     @Override
     public int setLayout() {
         return R.layout.activity_team_second;
@@ -26,13 +34,43 @@ public class TeamSecondActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        photoAdapter = new TeamPhotoAdapter(this);
-        lRecyclerView = bindView(R.id.discovery_team_num_rv);
+        rv = bindView(R.id.discovery_team_num_rv);
         data = new ArrayList<>();
+        rv.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
+        adapter = new TeamPhotoAdapter(this);
+        lRecyclerViewAdapter = new LRecyclerViewAdapter(adapter);
     }
 
     @Override
     public void initData() {
+        getTeamPhoto();
+    }
 
+    private void getTeamPhoto() {
+        String url = UrlTools.DISCOVERY_TEAM_NUM_PHOTO;
+        NetTool.getInstance().startRequest(url, TeamNumPhotoBean.class, new onHttpCallBack<TeamNumPhotoBean>() {
+            @Override
+            public void onSuccess(TeamNumPhotoBean response) {
+                data = response.getMembers();
+                Log.d("ssss", "data:" + data);
+                adapter.setData(data);
+                rv.setAdapter(lRecyclerViewAdapter);
+                LinearLayoutManager manager = new LinearLayoutManager(TeamSecondActivity.this,LinearLayoutManager.HORIZONTAL,false);
+                rv.setLayoutManager(manager);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+        });
     }
 }
+
+
+
+
+
+
+
